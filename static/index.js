c3f01a7384558e9d4f18500c84c3fb9b50491c93
AbNormal4820 <98322472+AbNormal4820@users.noreply.github.com>
2022-01-26 11:58:44 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,9 +341,9 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
+    if (score <= 49) return '这破玩意儿';
+    if (score <= 99) return '我都讲过多少遍了';
+    if (score <= 149) return '很有意思';
     if (score <= 199) return '您';
     return '人？';
 }
